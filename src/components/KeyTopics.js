280bba334733d2959292cce102b63c9baa365898
Amal <ken940005@gmail.com>
2024-09-26 18:57:09 +0530

    new topic added

--- src/components/KeyTopics.js
+++ src/components/KeyTopics.js
@@ -5,6 +5,7 @@ import data_m from "../assets/images/data_m.jpg";
 import bpm from "../assets/images/bpm.jpg";
 import bi from "../assets/images/bi.webp";
 import api from "../assets/images/api.png";
+import realtime from "../assets/images/realtime.jpg";
 
 
 const KeyTopics = () => {
@@ -62,6 +63,15 @@ const KeyTopics = () => {
         <div className='md:w-1/2 flex flex-col gap-y-10 bg-slate-100 rounded-xl shadow-2xl'>
           <img className='rounded-xl w-full h-[300px] object-cover shadow-2xl' src={api} alt='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnnZ-M6_2xgUWKt5mM5KS-y5rkuFv0Rle8Iw&s'/>
           <h1 className='w-full flex justify-center items-center text-2xl'><HighlightText text="API Management" /></h1>
+          <p className='p-5 mt-[-3rem] text-black mb-10'>
+            API Management enables secure, scalable, and efficient integration of applications, ensuring seamless connectivity and data flow across systems.
+          </p>
+        </div>
+      </div>
+      <div className='w-full flex gap-x-10 gap-y-10 flex-col md:flex-row mt-20 justify-center'>
+        <div className='md:w-1/2 flex flex-col gap-y-10 bg-slate-100 rounded-xl shadow-2xl'>
+          <img className='rounded-xl w-full h-[300px] object-cover shadow-2xl' src={realtime} alt='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnnZ-M6_2xgUWKt5mM5KS-y5rkuFv0Rle8Iw&s'/>
+          <h1 className='w-full flex justify-center items-center text-2xl'><HighlightText text="Real Time Analytics" /></h1>
           <p className='p-5 mt-[-3rem] text-black mb-10'>
             API Management enables secure, scalable, and efficient integration of applications, ensuring seamless connectivity and data flow across systems.
           </p>
