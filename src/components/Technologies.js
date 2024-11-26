4009f2c038abcd723ab9cabcfce791c33c3fdcf6
AmalRoy55 <amal@ethan-ai.com>
2024-11-26 11:06:59 +0530

    new changes

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -4,6 +4,7 @@ import workatoLogo from "../assets/images/workato UIC.svg";
 import solaceLogo from "../assets/images/solaceUIC.svg";
 import denodoLogo from "../assets/images/denodoUIC.svg";
 import kissflowLogo from "../assets/images/kissflowUIC.svg";
+import salesforce from "../assets/images/Salesforce.com_logo.png";
 // import dbtLogo from "../assets/images/dbtUIC.svg";
 // import exalateLogo from "../assets/images/exalateUIC.svg";
 import cleverflowLogo from "../assets/images/CleverflowLogoImageUIC.avif";
@@ -17,7 +18,7 @@ const Technologies = () => {
     <div className="bg-white max-w-[1250px] py-14 px-8  mt-10 flex flex-col">
       <div className="flex flex-col gap-4 justify-center items-center">
         <h1 className="lg:w-[100%] text-black text-center uppercase text-4xl font-mullish font-bold">
-        
+
           Our Technology
           <HighlightText text=" Partners" />
         </h1>
@@ -34,14 +35,15 @@ const Technologies = () => {
         <img src={solaceLogo} alt="Solace" className="h-32 w-auto md:mr-20 md:ml-10" />
         <img src={denodoLogo} alt="Denodo" className="h-16 w-auto mb-5" />
         {/* <img src={dbtLogo} alt="dbt" className="h-10 w-auto" /> */}
-        <img src={logo11} alt="exalate" className="h-20 w-auto" />
-        <div className="flex gap-2 justify-end items-end text-black ml-20 ">
+        <img src={logo11} alt="exalate" className="h-16 w-auto md:-ml-10" />
+        <img src={salesforce} alt="exalate" className="h-28 w-auto md:ml-20" />
+        <img src={kissflowLogo} alt="KissFlow" className="h-80 w-auto " />
+        <div className="flex gap-2 justify-end items-end text-black">
           <img src={cleverflowLogo} alt="cleverflow" className="h-16 w-auto" />
           <p className="text-2xl sm:text-2xl md:text-5xl mb-2  font-bold font-sans  text-center text-black">
             CleverFlow
           </p>
         </div>
-        <img src={kissflowLogo} alt="KissFlow" className="h-80 w-auto mr-26 mr-8" />
         {/* <img src={odooLogo} alt="odoo" className="h-11 w-auto ml-48 mb-1" /> */}
       </div>
     </div>
